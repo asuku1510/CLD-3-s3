f9a9bb1553beafd7dc458c4bba9eebf051aefa20
Sunday Asuku <sunday.asuku@gmail.com>
2023-09-29 00:24:43 -0500

    Scope / Description with fix, feat (SemVer) Contributing-to: https://sunnyasuku99.atlassian.net/browse/<JIRA-TICKET> Problem: fix Solution Note

---
+++ s3.tf
@@ -0,0 +1,40 @@
+#bootcamp32-${env}-${random.int}
+#s3 bucket for terraform
+resource "aws_s3_bucket" "backend" {
+  bucket = "bootcamp32-${lower(var.env)}-${random_integer.backend.result}"
+
+  tags = {
+    Name        = "My backend"
+    Environment = "Dev"
+  }
+}
+
+
+#kms key for bucket encryption
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "backend" {
+  bucket = aws_s3_bucket.backend.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
+
+#random integer for bucket naming convention
+resource "random_integer" "backend" {
+  min = 1
+  max = 100
+  keepers = {
+    # Generate a new integer each time we switch to a new listener ARN
+    Environment = var.env
+  }
+}
